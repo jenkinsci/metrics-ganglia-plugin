8703a9b86ebf976e6a4c0e1f948bfcc1c7441c4f
Stephen Connolly <stephen.alan.connolly@gmail.com>
2014-02-19 17:09:26 +0000

    Initial commit

---
+++ src/main/java/com/codahale/metrics/jenkins/ganglia/GangliaServer.java
@@ -0,0 +1,133 @@
+package com.codahale.metrics.jenkins.ganglia;
+
+import hudson.Extension;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import javax.annotation.concurrent.GuardedBy;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Stephen Connolly
+ */
+public class GangliaServer extends AbstractDescribableImpl<GangliaServer> {
+
+    private static final Logger LOGGER = Logger.getLogger(GangliaServer.class.getName());
+    private final String hostname;
+
+    private final int port;
+
+    private final String prefix;
+
+    @DataBoundConstructor
+    public GangliaServer(String hostname, int port, String prefix) {
+        this.hostname = hostname;
+        this.port = port;
+        this.prefix = prefix;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GraphiteServer{");
+        sb.append("hostname='").append(hostname).append('\'');
+        sb.append(", port=").append(port);
+        sb.append(", prefix='").append(prefix).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        GangliaServer that = (GangliaServer) o;
+
+        if (port != that.port) {
+            return false;
+        }
+        if (hostname != null ? !hostname.equals(that.hostname) : that.hostname != null) {
+            return false;
+        }
+        if (prefix != null ? !prefix.equals(that.prefix) : that.prefix != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = hostname != null ? hostname.hashCode() : 0;
+        result = 31 * result + port;
+        result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+        return result;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends Descriptor<GangliaServer> {
+        @GuardedBy("this")
+        private List<GangliaServer> servers;
+
+        @Override
+        public String getDisplayName() {
+            return Messages.GangliaServer_displayName();
+        }
+
+        public DescriptorImpl() {
+            super();
+            load();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            setServers(req.bindJSONToList(GangliaServer.class, json.get("servers")));
+            return true;
+        }
+
+        public synchronized List<GangliaServer> getServers() {
+            return servers == null
+                    ? Collections.<GangliaServer>emptyList()
+                    : Collections.unmodifiableList(new ArrayList<GangliaServer>(servers));
+        }
+
+        public synchronized void setServers(List<GangliaServer> servers) {
+            this.servers = servers;
+            save();
+            try {
+                Jenkins.getInstance().getPlugin(PluginImpl.class).updateReporters();
+            } catch (IOException e) {
+                LOGGER.log(Level.WARNING, "Could not update Graphite reporters", e);
+            } catch (URISyntaxException e) {
+                LOGGER.log(Level.WARNING, "Could not update Graphite reporters", e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/codahale/metrics/jenkins/ganglia/PluginImpl.java
@@ -0,0 +1,98 @@
+package com.codahale.metrics.jenkins.ganglia;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.ganglia.GangliaReporter;
+import com.codahale.metrics.jenkins.Metrics;
+import hudson.Plugin;
+import info.ganglia.gmetric4j.gmetric.GMetric;
+import jenkins.model.Jenkins;
+import jenkins.model.JenkinsLocationConfiguration;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Stephen Connolly
+ */
+public class PluginImpl extends Plugin {
+    private static final Logger LOGGER = Logger.getLogger(PluginImpl.class.getName());
+
+    private transient Map<GangliaServer, GangliaReporter> reporters;
+
+    public PluginImpl() {
+        this.reporters = new LinkedHashMap<GangliaServer, GangliaReporter>();
+    }
+
+    @Override
+    public void start() throws Exception {
+    }
+
+    @Override
+    public synchronized void stop() throws Exception {
+        if (reporters != null) {
+            for (GangliaReporter r : reporters.values()) {
+                r.stop();
+            }
+            reporters.clear();
+        }
+    }
+
+    @Override
+    public synchronized void postInitialize() throws Exception {
+        updateReporters();
+    }
+
+    public synchronized void updateReporters() throws URISyntaxException, IOException {
+        if (reporters == null) {
+            reporters = new LinkedHashMap<GangliaServer, GangliaReporter>();
+        }
+        MetricRegistry registry = Metrics.metricRegistry();
+        if (registry == null) {
+            return;
+        }
+        GangliaServer.DescriptorImpl descriptor =
+                Jenkins.getInstance().getDescriptorByType(GangliaServer.DescriptorImpl.class);
+        if (descriptor == null) {
+            return;
+        }
+        String url = JenkinsLocationConfiguration.get().getUrl();
+        URI uri = url == null ? null : new URI(url);
+        String hostname = uri == null ? "localhost" : uri.getHost();
+        Set<GangliaServer> toStop = new HashSet<GangliaServer>(reporters.keySet());
+        for (GangliaServer s : descriptor.getServers()) {
+            toStop.remove(s);
+            if (reporters.containsKey(s)) continue;
+            GMetric g = new GMetric(s.getHostname(), s.getPort(), GMetric.UDPAddressingMode.MULTICAST, 1);
+            String prefix = StringUtils.isBlank(s.getPrefix()) ? hostname : s.getPrefix();
+            GangliaReporter r = GangliaReporter.forRegistry(registry)
+                    .prefixedWith(prefix)
+                    .convertRatesTo(TimeUnit.MINUTES)
+                    .convertDurationsTo(TimeUnit.SECONDS)
+                    .filter(MetricFilter.ALL)
+                    .build(g);
+            reporters.put(s, r);
+            LOGGER.log(Level.INFO, "Starting Ganglia reporter to {0}:{1} with prefix {2}", new Object[]{
+                    s.getHostname(), s.getPort(), prefix
+            });
+            r.start(1, TimeUnit.MINUTES);
+        }
+        for (GangliaServer s: toStop) {
+            GangliaReporter r = reporters.get(s);
+            reporters.remove(s);
+            r.stop();
+            LOGGER.log(Level.INFO, "Stopped Ganglia reporter to {0}:{1} with prefix {2}", new Object[]{
+                    s.getHostname(), s.getPort(), StringUtils.isBlank(s.getPrefix()) ? hostname : s.getPrefix()
+            });
+        }
+    }
+}
